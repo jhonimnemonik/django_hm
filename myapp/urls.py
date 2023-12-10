9a87991c00a204bc2f404c3af055876fcc3e5cbe
jhonimnemonik <140925170+jhonimnemonik@users.noreply.github.com>
2023-12-10 09:59:17 +0100

    add contact form for views and urls

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,5 +5,6 @@ from myapp import views as page
 urlpatterns = [
     path('home/', page.main, name='home'),
     path('', lambda request: redirect('home')),
+    path('contact/contact_process.php', page.contact_process_view, name='contact_process'),
     path('contact/', page.contact, name='contact'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -5,5 +5,14 @@ def main(request):
     return render(request, 'home.html', {"page": "home"})
 
 
+def contact_process_view(request):
+    data = request.POST
+    print(data)
+    return render(request, 'contact.html', {"page": "contact"})
+
+
 def contact(request):
+    if request == "POST":
+        data = request.POST
+        print(data)
     return render(request, 'contact.html', {"page": "contact"})
